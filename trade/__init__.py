0a45e0bab483a7653d340b037110a2ed992b9bde
MinhTamNT <minhtam78945@gmail.com>
2024-09-09 11:57:42 +0700

    chore : update config

--- trade/__init__.py
+++ trade/__init__.py
@@ -8,24 +8,16 @@ import os
 
 load_dotenv()
 
-user_name = os.getenv('USER')
-password = os.getenv('PASSWORD')
-host = os.getenv('HOST')
-db = os.getenv('DBNAME')
-port_db = os.getenv('DB_PORT')
+# user_name = os.getenv('USER')
+# password = os.getenv('PASSWORD')
+# host = os.getenv('HOST')
+# db = os.getenv('DBNAME')
+# port_db = os.getenv('DB_PORT')
 
 app = Flask(__name__)
 app.secret_key = '*(&*(@*&(*@(^!(*@75876528378932^@%*&^(*@*@&#*'
 app.config["SQLALCHEMY_DATABASE_URI"] = (
-    "mysql+pymysql://{user}:{password}@{host}:{port}/{database}?charset=utf8mb4"
-    .format(
-        user=user_name,
-        password=quote(password),
-        host=host,
-        port=3307,
-        database=db
-    )
-)
+    f"mysql+pymysql://dev_app:%s@116.108.91.115:3307/app-trading?charset=utf8mb4") % quote("dev@1234")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 app.config['SECRET_KEY'] = '38a5f73c-ff0f-4b81-b466-701071019a4d'
 
